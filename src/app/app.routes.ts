c2c17de4b65fe5e90e89ec88292267fd256db0f9
Lucas Hackbart Döhnert <33258938+Lucashackd@users.noreply.github.com>
2026-03-13 18:21:32 -0300

    Add Angular SSR setup and new feature pages
    Add server-side rendering support and initial feature pages. Introduces SSR config and routes (src/main.server.ts, src/server.ts, src/app/app.config.server.ts, src/app/app.routes.server.ts), updates angular.json for server build/SSR, and adds a serve:ssr npm script. Also adds feature stubs (home, services, booking, contact) with component, template, style and spec files, and updates app config/routes and tsconfig for the SSR build. Dependency additions were made in package.json to support platform-server, @angular/ssr and Express (and related types).

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,21 @@
 import { Routes } from '@angular/router';
+import Home from './features/home/home';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    component: Home,
+  },
+  {
+    path: 'servicos',
+    loadComponent: () => import('./features/services/services')
+  },
+  {
+    path: 'contato',
+    loadComponent: () => import('./features/contact/contact')
+  },
+  {
+    path: 'agendamento',
+    loadComponent: () => import('./features/booking/booking')
+  }
+];
